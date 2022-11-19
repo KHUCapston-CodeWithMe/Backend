dfd4d46a14541a2e538a86b3d3b5419d572ce49b
Jeong <63828202+JeongA-Shin@users.noreply.github.com>
2022-11-19 22:18:59 +0900

    [Resolves #15] 채팅방 기본 코드 구현 (#16)
    * chore: websocket 의존성 추가
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * chore : websoscketconfig 추가
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * chore : websocsketconfig 설정
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * chore : 기타 환경 설정
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * feat : 채팅 관련 domain, dto 추가
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * feat : 채팅 관련 repository 추가
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * feat : 채팅 서비스 및 컨트롤러 추가
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * feat : session disconnet 관련 이벤트 리스너
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * feat : websocket chatting 관련 도메인
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * chore : local config
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * test : websocket test code
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * fix : chatting dto 수정
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * fix : 전반적으로 코드 이해 및 수정
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * feat : 소켓으로 채팅방 구현 테스트 코드 추가
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * feat : webrtc 관련 설정 추가
    
    https://github.com/KHUCapston-concoder/Backend/issues/15
    
    * feat : webrtc 연결 구현
    
    https://github.com/KHUCapston-concoder/Backend/issues/15

---
+++ src/main/java/oncoding/concoder/config/WebSocketConfig.java
@@ -0,0 +1,47 @@
+package oncoding.concoder.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Configuration
+@EnableWebSocketMessageBroker //웹 소켓 메시지 처리 활성화
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer { //WebSocketConfigurer
+
+  @Override
+  public void configureMessageBroker(final MessageBrokerRegistry registry) {
+    // 메세지 브로커는 아래 주소로 시작하는 주소의 Subscriber 에게 메시지를 전달하는 역할을 한다.
+    // 일반적으로 /topic은 한 명이 메시지를 발행했을 때, 해당 토픽을 구독하고 있는 n명에게 메시지를 뿌릴 때 사용
+    // 반면에 /queue는 한 명이 메시지를 발행했을 때, 발행한 한 명에게 다시 정보를 보내는 경우
+    registry.enableSimpleBroker("/sub");
+    // 클라이언트가 서버로 메시지를 보낼 때 아래 주소를 붙인다.
+    //즉 도착 경로에 대한 prefix 설정
+    //예를 들어 /app으로 설정해두면 /topic/hello라는 토픽에 대해 구독을 신청했을 때, 실제 경로는 /app/topic/hello가 되는 것
+    registry.setApplicationDestinationPrefixes("/pub");
+  }
+
+  @Override
+  public void registerStompEndpoints(final StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws-connection") // 클라이언트가 웹 소켓 요청을 하고 싶을 때 해당 End point 로 요청을 보낸다.
+        .setAllowedOrigins("*")
+        .withSockJS(); // fallback
+  }
+
+  @EventListener
+  public void onDisconnectEvent(final SessionDisconnectEvent event) {
+    System.out.println("DisconnectEvent");
+  }
+
+//  @Override
+//  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//
+//    registry.addHandler(new SocketHandler(), "/socket")
+//        .setAllowedOrigins("*");
+//  }
+}
---
+++ src/main/java/oncoding/concoder/service/ChattingService.java
@@ -0,0 +1,92 @@
+package oncoding.concoder.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import oncoding.concoder.dto.ChatDTO.DummyResponse;
+import oncoding.concoder.dto.ChatDTO.ExitResponse;
+import oncoding.concoder.dto.ChatDTO.MessageRequest;
+import oncoding.concoder.dto.ChatDTO.MessageResponse;
+import oncoding.concoder.dto.ChatDTO.SessionRequest;
+import oncoding.concoder.dto.ChatDTO.SessionResponse;
+import oncoding.concoder.model.Room;
+import oncoding.concoder.model.Session;
+import oncoding.concoder.model.User;
+import oncoding.concoder.repository.RoomRepository;
+import oncoding.concoder.repository.SessionRepository;
+import oncoding.concoder.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ChattingService {
+
+  private final UserRepository userRepository;
+  private final RoomRepository roomRepository;
+  private final SessionRepository sessionRepository;
+
+
+  /**
+   * 
+   * @param request
+   * @return request 내용을 바탕으로 생성된 메시지의 responseDTO 리턴
+   */
+  public MessageResponse sendMessage(final MessageRequest request) {
+    User user = userRepository.findById(request.getUserId()).orElseThrow(IllegalArgumentException::new);
+    return new MessageResponse(user.getId(), request.getContent()); //보낼 메세지 객체를 리턴
+  }
+
+  /**
+   * 
+   * @param roomId
+   * @param request
+   * @return sessionResponse - 결론적으로 roomId에 해당되는 users임
+   */
+
+  public SessionResponse enter(final UUID roomId, final SessionRequest request) {
+    User user = userRepository.findById(request.getUserId()).orElseThrow(IllegalArgumentException::new);
+    Room room = roomRepository.findById(roomId).orElseThrow(IllegalArgumentException::new);
+    Session session = new Session(request.getSessionId(), user, room); //세션 생성
+
+    sessionRepository.save(session); //여기서 세션의 찐 id까지 저장됨
+
+    return SessionResponse.from(room.users()); //sessionResponse 생성 - room의 users를 가지고 있음
+  }
+
+  /**
+   *
+   * @param sessionId
+   * @return 해당 sessionId가 있었던, 즉 나가는 방에서 해당 세션 삭제 후의 유저들을 반환
+   */
+  public ExitResponse exit(final String sessionId) {
+    Session session = sessionRepository.findBySessionId(sessionId).orElseThrow(IllegalArgumentException::new);
+    Room room = session.getRoom();//해당 session을 가지고 있는 room 찾음
+
+    session.delete(); //해당 session을 가지고 있는 user에서 session을 null로 변경, room이 가지고 있는 session 리스트에서 해당 session을 없앰
+    sessionRepository.delete(session); //session자체를 삭제
+
+    return new ExitResponse(room.getId(), SessionResponse.from(room.users()));
+  }
+
+
+  public DummyResponse createDummy() {
+    List<User> users = new ArrayList<>();
+    List<Room> rooms = new ArrayList<>();
+
+    users.add(userRepository.save(new User("와일더")));
+    users.add(userRepository.save(new User("마이클")));
+    users.add(userRepository.save(new User("제이슨")));
+    users.add(userRepository.save(new User("오스카")));
+
+    rooms.add(roomRepository.save(new Room(2)));
+    rooms.add(roomRepository.save(new Room(4)));
+    rooms.add(roomRepository.save(new Room(5)));
+
+    return DummyResponse.of(users, rooms);
+  }
+
+}
