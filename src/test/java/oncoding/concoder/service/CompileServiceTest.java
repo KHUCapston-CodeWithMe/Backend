c9de943c93ad48639cb7f2d43687460dd021129f
Subin Min <tnqls5417@naver.com>
2022-11-20 21:03:08 +0900

    test: Task 인풋 처리 테스트케이스 작성

--- src/test/java/oncoding/concoder/service/CompileServiceTest.java
+++ src/test/java/oncoding/concoder/service/CompileServiceTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -78,4 +80,42 @@ class CompileServiceTest {
         assertThat(future).succeedsWithin(THREAD_TIMEOUT_SECONDS, TimeUnit.SECONDS);
     }
 
+    @Test
+    void 인풋_받는_Task_실행() {
+        // given
+        String inputCode = "a=int(input())\n"
+            +"print(a)\n"
+            +"b=int(input())\n"
+            +"print(b)";
+        List<String> inputs = List.of("5\n15", "7\n21");
+        List<String> expectedResults = List.of("5\n15", "7\n21");
+
+        // when
+        List<String> results = new ArrayList<>();
+        List<Future<String>> futures = new ArrayList<>();
+        try {
+            for (int i = 0; i<inputs.size(); i++) {
+                Future<String> future = compileService.run(inputCode, inputs.get(i));
+                futures.add(future);
+            }
+            for (int i = 0; i<inputs.size(); i++) {
+                results.add(futures.get(i).get());
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+        // then
+        assertThat(results.size()).isEqualTo(inputs.size()); // Error 내용 반환
+        for (int i = 0; i<inputs.size(); i++) {
+            Future<String> future = futures.get(i);
+            assertThat(future).isNotNull();
+            assertThat(future).isDone(); // Task 자체는 시간 내 완료
+            assertThat(future).succeedsWithin(THREAD_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+            String result = results.get(i);
+            assertThat(result).isEqualTo(expectedResults.get(i));
+        }
+
+    }
 }
