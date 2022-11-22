0cf2b92ed05994f419f85364b8391845dabb92c1
신정아 <jeonga@khu.ac.kr>
2022-11-23 01:20:46 +0900

    fix : sessionId값 header로 받아오게

--- src/main/java/oncoding/concoder/controller/VideoRoomController.java
+++ src/main/java/oncoding/concoder/controller/VideoRoomController.java
@@ -23,7 +23,6 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
