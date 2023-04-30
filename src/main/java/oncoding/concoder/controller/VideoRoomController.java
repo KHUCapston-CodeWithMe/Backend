f16fae79d5d928e278e70bd3681b91c83950afb4
sschan99 <seungchan1219@gmail.com>
2023-04-30 11:20:42 +0900

    feat: Session 초기화 및 Connection 생성 구현

--- src/main/java/oncoding/concoder/controller/VideoRoomController.java
+++ src/main/java/oncoding/concoder/controller/VideoRoomController.java
@@ -1,9 +1,18 @@
 package oncoding.concoder.controller;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
+import io.openvidu.java.client.Connection;
+import io.openvidu.java.client.ConnectionProperties;
+import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
+import io.openvidu.java.client.Session;
+import io.openvidu.java.client.SessionProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import oncoding.concoder.dto.ChatDTO.DummyResponse;
@@ -14,7 +23,9 @@ import oncoding.concoder.dto.ChatDTO.SessionResponse;
 import oncoding.concoder.dto.ChatDTO.UserResponse;
 import oncoding.concoder.service.ChattingService;
 import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -23,6 +34,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +55,15 @@ public class VideoRoomController {
 
   private Map<UUID, Object> usersAtRooms;
 
+  @Value("${OPENVIDU_SECRET}")
+  private String OPENVIDU_SECRET;
+
+  private OpenVidu openvidu;
+
+  @PostConstruct
+  public void init() {
+    this.openvidu = new OpenVidu("http://localhost:4443/", OPENVIDU_SECRET);
+  }
 
 
   @MessageMapping("/video/chat/{roomId}")
@@ -134,7 +155,35 @@ public class VideoRoomController {
 
   }
 
+  /**
+   * @param params The Session properties
+   * @return The Session ID
+   */
+  @PostMapping("/sessions")
+  public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
+          throws OpenViduJavaClientException, OpenViduHttpException {
+    SessionProperties properties = SessionProperties.fromJson(params).build();
+    Session session = openvidu.createSession(properties);
+    return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+  }
 
+  /**
+   * @param sessionId The Session in which to create the Connection
+   * @param params    The Connection properties
+   * @return The Token associated to the Connection
+   */
+  @PostMapping("/sessions/{sessionId}/connections")
+  public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
+                                                 @RequestBody(required = false) Map<String, Object> params)
+          throws OpenViduJavaClientException, OpenViduHttpException {
+    Session session = openvidu.getActiveSession(sessionId);
+    if (session == null) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+    Connection connection = session.createConnection(properties);
+    return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+  }
 
 
   // caller의 정보를 다른 callee들에게 쏴준다.
